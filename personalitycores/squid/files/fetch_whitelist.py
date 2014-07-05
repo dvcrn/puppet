19e46558e50c6e94039ac6b44032ea7b1b556e90
David Mohl <me@dave.cx>
2014-07-05 18:49:55 +0900

    absolute path

--- personalitycores/squid/files/fetch_whitelist.py
+++ personalitycores/squid/files/fetch_whitelist.py
@@ -6,5 +6,5 @@ request = urllib.request.urlopen("http://api.proxmate.me/proxy/whitelist.json")
 json_string = request.read()
 whitelist_entries = json.loads(json_string.decode("utf-8"))
 
-whitelist_file = open("./whitelist", encoding="utf-8", mode="w")
+whitelist_file = open("/etc/squid/whitelist", encoding="utf-8", mode="w")
 whitelist_file.write('\n'.join(whitelist_entries))
