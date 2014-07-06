36a98ba1911a3de168af698d1169860ffbc90f66
David Mohl <me@dave.cx>
2014-07-06 19:53:04 +0900

    added squid reload to whitelist fetch

--- personalitycores/squid/files/fetch_whitelist.py
+++ personalitycores/squid/files/fetch_whitelist.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from subprocess import call
 import urllib.request
 import json
 
@@ -8,3 +9,4 @@ whitelist_entries = json.loads(json_string.decode("utf-8"))
 
 whitelist_file = open("/etc/squid/whitelist", encoding="utf-8", mode="w")
 whitelist_file.write('\n'.join(whitelist_entries))
+call(["service", "squid reload"])
